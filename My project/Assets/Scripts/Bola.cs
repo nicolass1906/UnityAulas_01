f8e6e7e4a069a625722fd39e08e55f965d89345f
nicolass1906 <163528826+nicolass1906@users.noreply.github.com>
2024-04-12 09:34:33 -0300

    Atualização final
    Atualização final do jogo do pong

--- My project/Assets/Scripts/Bola.cs
+++ My project/Assets/Scripts/Bola.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Bola : MonoBehaviour
 {
     public float velocidade = 10;
+    
+    public TMP_Text pontuacaoEsquerda;
+    public TMP_Text pontuacaoDireita;
+    int pontosEsquerda = 0;
+    int pontosDireita = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,12 +31,32 @@ public class Bola : MonoBehaviour
         float y = Random.Range(0, 2) == 0 ? -1 : 1;
 
         GetComponent<Rigidbody>().velocity = new Vector2(velocidade*x,velocidade*y);
-         
+
+        pontuacaoDireita.SetText(pontosDireita.ToString());
+        pontuacaoEsquerda.SetText(pontosEsquerda.ToString());
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter(Collision Batida)
+    {
+        if ((Batida.gameObject.name == "BEsquerda"))
+        {
+            transform.position = new Vector3(2, 0, 9);
+            pontosDireita++;
+            Start();
+        }
+
+        else if((Batida.gameObject.name == "BDireita"))
+        {
+            transform.position = new Vector3(2, 0, 9);
+            pontosEsquerda++;
+            Start();
+        }
     }
 }
---
+++ My project/Assets/Scripts/jogador1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+public class Jogador1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Input.GetAxisRaw("Vertical2")>0) && (transform.position.y<7))
+        {
+            transform.Translate(0, 0.1f, 0);
+        }
+        else if((Input.GetAxisRaw("Vertical2")<0) && (transform.position.y>-5))
+        {
+            transform.Translate(0, -0.1f, 0);
+        }
+
+    }
+}
